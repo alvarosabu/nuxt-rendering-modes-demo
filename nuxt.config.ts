202cb1574eec47c86bf3bbd51f6d51d4ac8b6139
Alvaro <alvaro.saburido@gmail.com>
2022-05-13 17:08:58 +0200

    ssr back

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
   buildModules: ['@unocss/nuxt'],
   css: ['@alvarosabu/ui/dist/style.css'],
   unocss: {
